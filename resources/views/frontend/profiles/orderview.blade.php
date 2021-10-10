fa796e1c005bcc50c865ec294b3a43afc5f2dd71
hari450 <haribabu@xmediasolution.com>
2021-10-10 19:57:37 +0530

    user order history

--- resources/views/frontend/profiles/orderview.blade.php
+++ resources/views/frontend/profiles/orderview.blade.php
@@ -1,3 +1,4 @@
+
 @include('frontend.header')
 
 <link rel="stylesheet" href="{{ Url::to('assets/profile.css')}}" />
@@ -80,25 +81,17 @@
                 <div class="content px-3">
 
 
+    <div class="content px-3">
 
-                    <div class="container mt-5">
+        {!! $orderdetail !!}
 
-                        <table class="table table-bordered yajra-datatable">
-                            <thead>
-                                <tr>
-                                    <th>No</th>
-                                    <th>Name</th>
-                                    <th>Email</th>
-                                    <th>Address</th>
-                                    <th>Phone</th>
-                                    <th>Status</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            </tbody>
-                        </table>
-                    </div>
+
+        {!! $orderhistory !!}
+
+            </div>
+
+
+
 
                 </div>
             </div>
@@ -106,34 +99,12 @@
     </div>
 </div>
 
-<script>
-     $(function () {
-
-var table = $('.yajra-datatable').DataTable({
-    processing: true,
-    serverSide: true,
-    ajax: "{{ route('order.getuseOrders') }}",
-    columns: [
-        {data: 'DT_RowIndex', name: 'DT_RowIndex'},
-        {data: 'billing_name', name: 'billing_name'},
-        {data: 'billing_email', name: 'billing_email'},
-        {data: 'billing_city', name: 'billing_city'},
-        {data: 'billing_phone', name: 'billing_phone'},
-        {data: 'order_status_id', name: 'order_status_id' },
-        {
-            data: 'action',
-            name: 'action',
-            orderable: true,
-            searchable: true
-        },
-    ]
-});
-
-
-
-
-
-});
-</script>
 
 @include('frontend.footer')
+
+
+
+
+
+
+
