a99c613df88a544a531a5d6a59df477750f74cd9
hari <hari@hari-151.local>
2022-04-02 13:29:47 +0530

    meta

---
+++ resources/views/frontend/partnoheader.blade.php
@@ -0,0 +1,22 @@
+
+@include('frontend.partheader')
+
+
+<!--
+    <span class="clearfix">
+        <a href="{{ route('cartdata') }}">Cart</a>
+        <span class="basket-item-count">
+            <span class="badge badge-pill red"> 0 </span>
+        </span>
+    </span> -->
+
+
+    {{-- <div class="container-fluid"> --}}
+
+        @yield('content')
+    {{-- </div> --}}
+
+
+
+
+@include('frontend.footer')
